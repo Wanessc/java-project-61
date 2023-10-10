5f267e0d5b20e358227c40c405f2ea4f41223b0e
Maxim Ivanov <mausimivanov@gmail.com>
2023-10-10 21:20:35 +0300

    game

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 public class Cli {
     public static void greetUser() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("May I have your name? John");
-        System.out.println("Hello, John!");
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        String name = scanner.next();
+        System.out.println("Hello, " + name + "!");
 
-        scanner.close();
     }
 }
