8f0b02d3a46e470bcaad022950d26014a84c99fd
Maxim Ivanov <mausimivanov@gmail.com>
2023-10-14 22:33:43 +0300

    play

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,5 +1,4 @@
 package hexlet.code;
-import java.util.Random;
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
@@ -7,46 +6,19 @@ public class App {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
+        System.out.println("3 - Calc");
         System.out.println("0 - Exit");
 
         int choise = scanner.nextInt();
 
-        switch (choise) {
-            case(1) :
-                Cli.greetUser();
-                break;
-            case(0) :
-                System.out.println("Exit");
-                break;
-            case(2) :
-                Cli.greetUser();
-                Random random = new Random();
-                int maxAttempts = 3;
-                System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
-
-                int currentAnswer = 0;
-
-                while (currentAnswer < maxAttempts) {
-                    int number = random.nextInt(100);
-                    System.out.println("Question: " + number);
-                    System.out.println("Your answer:");
-                    String userAnswer = scanner.next();
-
-                    boolean isEven = number % 2 == 0;
-
-                    if (isEven && userAnswer.equals("yes") || (!isEven && userAnswer.equals("no"))) {
-                        System.out.println("Correct!");
-                        currentAnswer ++;
-                    } else {
-                        System.out.println(userAnswer + "  is wrong answer ;(. Correct answer was " + (isEven? "yes" : "'no" + "'."));
-                        System.out.println("Let's try again, Bill!");
-                        break;
-                    }
-                }
-                if (currentAnswer == maxAttempts) {
-                    System.out.println("Congratulations, Bill!");
-                    break;
-                }
+        if(choise == 1) {
+            Cli.greetUser();
+        } else if (choise == 2) {
+            FirstGame firstGame = new FirstGame();
+            firstGame.run();
+        } else if (choise == 3) {
+            SecondGame secondGame = new SecondGame();
+            secondGame.run();
         }
     }
 }
---
+++ app/src/main/java/hexlet/code/FirstGame.java
@@ -0,0 +1,44 @@
+package hexlet.code;
+
+import java.util.Random;
+import java.util.Scanner;
+    interface game {
+        void run();
+    }
+
+    class FirstGame implements game {
+        private final int maxNumber = 100;
+        private final int maxAttempts = 3;
+        private Random random = new Random();
+        private Scanner scanner = new Scanner(System.in);
+
+        @Override
+        public void run() {
+            System.out.println("Welcome to the Brain Games!");
+            System.out.println("May I have your name?");
+            String name = scanner.next();
+            System.out.println("Hello, " + name + "!");
+            System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+            int correctAnswers = 0;
+            while (correctAnswers < maxAttempts) {
+                int number = random.nextInt(maxNumber);
+                System.out.println("Question: " + number);
+                System.out.print("Your answer: ");
+                String userAnswer = scanner.next();
+
+                boolean isEven = number % 2 == 0;
+                if (isEven && userAnswer.equals("yes") || !isEven && userAnswer.equals("no")) {
+                    System.out.println("Correct!");
+                    correctAnswers++;
+                } else {
+                    System.out.println(userAnswer + " is wrong answer ;(. Correct answer was ' " + (isEven ? "'yes''" : "'no'") + ".");
+                    System.out.println("Let's try again, " + name +"!");
+                    return;
+                }
+            }
+            if (correctAnswers == maxAttempts) {
+                System.out.println("Congratulations, " + name + "!");
+            }
+        }
+    }
+
